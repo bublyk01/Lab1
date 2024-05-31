a4c6244169dfd2dfe5fb8b7d2cd07e54170042bc
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-05-31 12:33:35 +0300

    Matrix was implemented, along with transformation functions

--- code.py
+++ code.py
@@ -12,3 +12,19 @@ class Vector:
 
     def __linear__(self):
         return f"Vector(x={self.x}, y={self.y})"
+
+
+class Matrix:
+    def __init__(self, a, b, c, d):
+        self.matrix = np.array([[a, b], [c, d]])
+
+    def transform(self, vector):
+        result = self.matrix @ vector.to_array()
+        return Vector(result[0], result[1])
+
+    def points_transform(self, points):
+        transformed_points = self.matrix @ points.T
+        return transformed_points.T
+
+    def __repr__(self):
+        return f"Matrix(\n{self.matrix[0, 0]} {self.matrix[0, 1]}\n{self.matrix[1, 0]} {self.matrix[1, 1]}\n)"
