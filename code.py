c4e172aafb41ca17f713fbca557e8c3d09daf130
Volodya Bublyk <151150647+bublyk01@users.noreply.github.com>
2024-05-31 15:16:05 +0300

    User can now input the amount of degrees to rotate the shape

--- code.py
+++ code.py
@@ -15,8 +15,11 @@ class Vector:
 
 
 class Matrix:
-    def __init__(self, a, b, c, d):
-        self.matrix = np.array([[a, b], [c, d]])
+    def __init__(self, angle_deg):
+        angle_rad = np.deg2rad(angle_deg)
+        cos_angle = np.cos(angle_rad)
+        sin_angle = np.sin(angle_rad)
+        self.matrix = np.array([[cos_angle, -sin_angle], [sin_angle, cos_angle]])
 
     def transform(self, vector):
         result = self.matrix @ vector.array_convert()
@@ -39,7 +42,7 @@ class Shape:
         return Shape(transformed_points)
 
     def plot(self, ax, **kwargs):
-        points = np.vstack([self.points, self.points[0]])
+        points = np.vstack([self.points, self.points[0]])  # Close the shape by adding the first point at the end
         ax.plot(points[:, 0], points[:, 1], **kwargs)
 
     def __repr__(self):
@@ -50,13 +53,15 @@ if __name__ == "__main__":
     batman_points = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
     batman_shape = Shape(batman_points)
 
-    rotation_matrix = Matrix(0, -1, 1, 0)
+    degrees = float(input("Enter the angle in degrees: "))
+
+    rotation_matrix = Matrix(degrees)
 
     transformed_batman_shape = batman_shape.transform(rotation_matrix)
 
     fig, ax = plt.subplots()
-    batman_shape.plot(ax, color='yellow', label='Initial shape')
-    transformed_batman_shape.plot(ax, color='red', label='Transformed shape')
+    batman_shape.plot(ax, color='yellow', label='Original')
+    transformed_batman_shape.plot(ax, color='red', linestyle='--', label='Transformed')
     ax.set_aspect('equal')
     ax.legend()
     plt.show()
